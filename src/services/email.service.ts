4bc1a0bd821f72740c916faae7eff75207fef672
Ruslan <bik.big102rus@gmail.com>
2024-07-13 14:39:43 +0500

    refactor

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,16 +1,22 @@
 import axios from 'axios'
 import { IEmail } from '../pages/types'
+interface IData {
+	data: Array<{
+		id: string
+		text: string
+	}>
+}
 
 class EmailService {
 	private URL = 'http://localhost:3000/emails'
 
 	async getEmails() {
-		const { data } = await axios.get<IEmail[]>(this.URL)
+		const { data } = await axios.get<IEmail[]>(this.URL).catch(e => console.log(e))
 		return data
 	}
 
 	async createEmail(text: string) {
-		const { data } = await axios.post(this.URL, { text })
+		const { data } = await axios.post(this.URL, { text }).catch(e =>console.log(e))
 		return data
 	}
 }
